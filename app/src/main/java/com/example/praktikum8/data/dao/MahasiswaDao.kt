3c66eaf22c89235c3527058be540397fad731613
riskalaluna <riskalaluna@gmail.com>
2024-12-15 14:18:46 +0700

    menuliskan kode untuk getAllMahasiswa yang digunakan untuk mengambil semua data mahasiswa yang tersimpan di database

--- app/src/main/java/com/example/praktikum8/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/praktikum8/data/dao/MahasiswaDao.kt
@@ -2,10 +2,17 @@ package com.example.praktikum8.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.praktikum8.data.entity.Mahasiswa
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //getAllMahasiswa
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+
 }
