5898e5486dcb391ece156a9a803698556a7394a0
riskalaluna <riskalaluna@gmail.com>
2024-12-15 23:00:46 +0700

    bagian dari ViewModel yang bertugas mengambil data mahasiswa berdasarkan NIM dari repository, mengonversinya menjadi format MhsUIState, dan menyimpannya dalam updateUIState agar UI Jetpack Compose diperbarui secara otomatis. Data NIM diambil dari savedStateHandle, dan proses pengambilan data dilakukan secara asynchronous menggunakan coroutine pada viewModelScope.

--- app/src/main/java/com/example/praktikum8/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum8/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,12 +1,33 @@
 package com.example.praktikum8.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum8.repository.RepositoryMhs
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiEdit.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
 
 }
