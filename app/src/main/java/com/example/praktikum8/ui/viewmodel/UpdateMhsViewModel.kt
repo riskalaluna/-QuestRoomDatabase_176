47162c2e6c6d55ffdb60abb9ed72a1df4a1b1f29
riskalaluna <riskalaluna@gmail.com>
2024-12-15 22:55:32 +0700

    mendefinisikan UpdateMhsViewModel, sebuah kelas turunan dari ViewModel yang biasanya digunakan dalam arsitektur MVVM (Model-View-ViewModel) di Android untuk mengelola dan menyimpan data antarmuka pengguna dalam siklus hidup.

---
+++ app/src/main/java/com/example/praktikum8/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum8.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikum8.repository.RepositoryMhs
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+}
