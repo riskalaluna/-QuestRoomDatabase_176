a65d997e766cecacb64028409cf3d8351e0ba445
riskalaluna <riskalaluna@gmail.com>
2024-12-15 14:47:02 +0700

    menuliskan code getAllMhs untuk memanggil fungsi get AllMahasiswa dari MahasiswaDao untuk mendapatkan semua data mahasiswa

--- app/src/main/java/com/example/praktikum8/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/praktikum8/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.praktikum8.repository
 
 import com.example.praktikum8.data.dao.MahasiswaDao
 import com.example.praktikum8.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,11 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    //getAllMhs
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+
 }
