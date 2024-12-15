9c86d7b1c8f8f27f1810e3c5c75a082c737cf785
riskalaluna <riskalaluna@gmail.com>
2024-12-15 23:07:45 +0700

    Fungsi validateFields memvalidasi data mahasiswa dalam updateUIState dengan memastikan setiap field (NIM, nama, jenis kelamin, alamat, kelas, dan angkatan) tidak kosong, dan mencatat pesan error jika ada field yang tidak valid. Hasil validasi disimpan dalam isEntryValid dari updateUIState

--- app/src/main/java/com/example/praktikum8/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/praktikum8/ui/viewmodel/UpdateMhsViewModel.kt
@@ -30,4 +30,26 @@ class UpdateMhsViewModel(
         }
     }
 
+    fun updateState (mahasiswaEvent: MahasiswaEvent) {
+        updateUIState = updateUIState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
+
+    fun validateFields(): Boolean {
+        val event = updateUIState.mahasiswaEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+        )
+
+        updateUIState = updateUIState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+
 }
