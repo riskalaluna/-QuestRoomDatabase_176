e1d1ca0eca73c43610406cce23bfeed92e8ab2e8
riskalaluna <riskalaluna@gmail.com>
2024-12-16 12:13:25 +0700

    menuliskan code di fungsi HomeMhsView

--- app/src/main/java/com/example/praktikum8/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/praktikum8/ui/view/mahasiswa/HomeMhsView.kt
@@ -11,17 +11,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.internal.composableLambdaInstance
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -31,8 +37,54 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.praktikum8.data.entity.Mahasiswa
+import com.example.praktikum8.ui.viewmodel.HomeMhsViewModel
 import com.example.praktikum8.ui.viewmodel.HomeUiState
+import com.example.praktikum8.ui.viewmodel.UpdateMhsViewModel
 import kotlinx.coroutines.launch
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum8.ui.custumwidget.CstTopAppBar
+import com.example.praktikum8.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    Scaffold (
+        topBar = {
+            CstTopAppBar(
+                judul = "Daftar Mahasiswa",
+                showBackButton = false,
+                onBack = { },
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa",
+                )
+
+            }
+        }
+    )  { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding((innerPadding))
+        )
+    }
+}
 
 @SuppressLint("RememberReturnType")
 @Composable
