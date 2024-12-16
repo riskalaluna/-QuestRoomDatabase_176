6867b8337902c13482ffe25434d851c56b97d18d
riskalaluna <riskalaluna@gmail.com>
2024-12-16 11:53:40 +0700

    menampilkan indikator loadang dan menampilkan pesan error

--- app/src/main/java/com/example/praktikum8/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/praktikum8/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,8 +1,11 @@
 package com.example.praktikum8.ui.view.mahasiswa
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,16 +15,57 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.internal.composableLambdaInstance
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.praktikum8.data.entity.Mahasiswa
+import com.example.praktikum8.ui.viewmodel.HomeUiState
+import kotlinx.coroutines.launch
+
+@SuppressLint("RememberReturnType")
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: () -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } //snackbar state
+    when {
+        homeUiState.isLoading -> {
+            //menampilkan indikator loading
+            Box (
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError -> {
+            //menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) //tampilkan snackbar
+                    }
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun ListMahasiswa(
